80e113717b15202fe4d24e4f58fcce24388d7e54
Pawan <122356629+pawanbishnoi765@users.noreply.github.com>
2023-05-26 17:07:24 +0530

    IssueTrackerProject

---
+++ config/mongoose.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+// const url = 'mongodb://0.0.0.0:27017/db';
+const url = "mongodb://127.0.0.1:27017/issueTracker";
+// mongoose.connect('url') 
+mongoose.connect(url);
+const db = mongoose.connection;
+
+//If any Error then Getting this Line
+db.on('error', console.error.bind(console, "Error connecting to MongoDB"));
+
+
+db.once('open', () => {
+    console.log("Connected to Database :: MongoDB ")
+});
+
+module.exports = db;  //Exports db
